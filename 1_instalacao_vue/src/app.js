c26e1dce8ac9841e79addf02ca56b1bc8dd13a6e
richard <arthursartori27@gmail.com>
2024-09-06 23:12:38 -0300

    feito

--- 1_instalacao_vue/src/app.js
+++ 1_instalacao_vue/src/app.js
@@ -1,9 +1,22 @@
 const MyNameApp = {
     data() {
       return {
-        name: "arthur",
-        idade: 14
+        name: "",
+        age: 0,
+        input_name: "",
+        input_age: "",
       };
+    },
+    methods: {
+        submitForm(e) {
+
+            e.preventDefault();
+            console.log(this.input_name);
+            this.name = this.input_name
+
+            console.log(this.input_age);
+            this.age = this.input_age
+        }
     }
   };
   
